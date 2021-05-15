d693d8d80fe307755b997e9eb727d8ec104c59ae
Mst. Sadia khatun <73788572+sadia4444a@users.noreply.github.com>
2021-05-15 09:25:30 +0600

    Add files via upload

---
+++ int.py
@@ -0,0 +1,23 @@
+print(3+2)
+print(3-2)
+print(3*2)
+print(3**2)
+print(3%2)
+print(3/2)
+print(3//2) #floor division
+print(abs(-3.75))
+print(round(3.75))
+print(round(3.758,2))
+print(type(3.75))
+
+n1='100'
+n2='200'
+print(n1+n2)
+n1=int(n1)
+n2=int(n2)
+print(n1+n2)
+
+n1=100
+n2=300
+print(n1>n2)
+print(n1==n2)
